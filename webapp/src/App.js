52dda6e5221ce56c13e9073f94294d2206bfd696
baozhoutao <baozhoutao@hotoa.com>
2019-09-22 12:10:00 +0800

    优化store

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Provider } from 'react-redux';
+import { Provider  } from 'react-redux';
 import store from './stores/configureStore'
 import SelectUsers from './components/select_users'
 import Grid from './components/dx_grid'
 
 function App() {
-
+  let appStore = store({settings: {services: {odata: 'http://127.0.0.1:5000'}}})
   let columns= [
     // { name: '_id', title: '唯一标识' },
     { name: 'name', title: '名称' },
@@ -17,7 +17,7 @@ function App() {
   let rootNodes = ["51ae9b1a8e296a29c9000002"] // , "51aefb658e296a29c9000049"
   return (
     <div className="App">
-      <Provider store={store}>
+      <Provider store={appStore}>
         {/* <Grid objectName='instances' columns={columns} getRowId={getRowId}/> */}
         <SelectUsers getRowId={getRowId} rootNodes={rootNodes} valueField="user"/>
       </Provider>
