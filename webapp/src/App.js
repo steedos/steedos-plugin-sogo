a101198b9aa9a95e01e72c9d4b6fdff3cd623c19
baozhoutao <baozhoutao@hotoa.com>
2019-09-25 18:37:26 +0800

    实现回写数据功能

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,15 +3,10 @@ import './App.css';
 import { Provider  } from 'react-redux';
 import store from 'steedos-webapp/lib/stores/configureStore'
 import SelectUsers from 'steedos-webapp/lib/components/select_users'
+import _ from 'underscore'
 
 function App() {
   let appStore = store({settings: {services: {odata: 'http://127.0.0.1:5000'}}})
-  let columns= [
-    { name: '_id', title: '唯一标识' },
-    { name: 'name', title: '名称' },
-    // { name: 'applicant_name', title: '申请人' },
-    { name: 'email', title: 'Email'}
-  ];
 
   let selectionLabel = (item)=>{
     return `${item.name}(${item.email})`
@@ -19,16 +14,46 @@ function App() {
 
   let userListColumns = [
     { name: 'name', title: '姓名' },
-    // { name: 'username', title: 'username' },
     { name: 'email', title: '邮箱地址' },
     { name: 'mobile', title: '手机号' }
   ]
+
+
+  let getTagElement = ()=>{
+    const searchParams = new URLSearchParams(window.location.search);
+    return searchParams.get("tag_element")
+  }
+
+  let closeWindow = ()=>{
+    window.location.href="about:blank";
+    window.close();
+  }
+
+
+  let getSelectUsersSelection = ()=>{
+    return appStore.getState().entities.space_users.selection
+  }
+
+  let confirm = ()=>{
+    let selection = getSelectUsersSelection();
+    let tagElement = getTagElement()
+    //TODO 处理提示功能
+    if(_.isEmpty(selection)){
+      console.log('请选择收件人');
+    }
+    window.opener.document.getElementById(tagElement).value = _.pluck(selection, "email");
+    closeWindow();
+  }
 
   return (
     <div className="App">
       <Provider store={appStore}>
         <SelectUsers selectionLabel={selectionLabel} userListColumns={userListColumns}/>
       </Provider>
+      <div className="slds-docked-form-footer">
+      <button type="button" className="slds-button slds-button_neutral" onClick={closeWindow}>取消</button>
+      <button type="button" className="slds-button slds-button_brand" onClick={confirm}>确认</button>
+    </div>
     </div>
   );
 }
